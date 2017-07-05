12b6c293ff90ca6548ade373a3a586fd668ea000
vincent <vincent@localhost>
2017-07-05 09:41:12 +0800

    更新

--- bizhub-kafka-producer/src/main/java/com/bizhub/controller/MainController.java
+++ bizhub-kafka-producer/src/main/java/com/bizhub/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.bizhub.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,14 +19,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 import com.bizhub.common.TopicEnum;
-import com.bizhub.entity.Item;
 import com.bizhub.kafka.producer.KafkaProducerService;
 
 @Controller
 public class MainController {
 
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
-    
+
     @Autowired
     private KafkaProducerService kafkaService;
 
@@ -32,20 +34,61 @@ public class MainController {
         log.info("# 进入默认首页");
         return "index";
     }
-    
+
+    @SuppressWarnings("unchecked")
     @RequestMapping(value = "send", method = RequestMethod.POST)
-    @ResponseBody String send() {
-        Item item = new Item();
-        item.setItemId("0222000300000000003");
-        item.setBrandName("波司登");
-        item.setBusiName("波司登");
-        item.setGoodsName("波司登T恤");
-        String message = JSON.toJSONString(item);
+    @ResponseBody
+    String send(HttpServletRequest request) {
+        String serverName = request.getServerName();
+        Integer serverPort = request.getServerPort();
+        String serverPath = request.getContextPath();
+
+        String localName = request.getLocalName();
+        String localAddr = request.getLocalAddr();
+        Integer localPort = request.getLocalPort();
+
+        String remoteHost = request.getRemoteHost(); // 客户端主机名
+        String remoteAddr = request.getRemoteAddr(); // 客户端ip地址
+        Integer remotePort = request.getRemotePort(); // 客户端端口号
+
+        String protocol = request.getProtocol();
+        String schema = request.getScheme();
+
+        String method = request.getMethod();
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        String queryString = request.getQueryString();
+
+        String requestURI = request.getRequestURI();
+        String requestURL = request.getRequestURL().toString();
+
+        // 构造 map
+        Map<String, Object> map = new HashMap<>();
+        // map.put("requestHeaderMap", requestHeaderMap);
+        // map.put("responseHeaderMap", responseHeaderMap);
+        map.put("serverName", serverName);
+        map.put("serverPort", serverPort);
+        map.put("serverPath", serverPath);
+        map.put("localName", localName);
+        map.put("localAddr", localAddr);
+        map.put("localPort", localPort);
+        map.put("remoteHost", remoteHost);
+        map.put("remoteAddr", remoteAddr);
+        map.put("remotePort", remotePort);
+        map.put("protocol", protocol);
+        map.put("schema", schema);
+        map.put("method", method);
+        map.put("parameterMap", parameterMap);
+        map.put("queryString", queryString);
+        map.put("requestURI", requestURI);
+        map.put("requestURL", requestURL);
+        map.put("visitDate", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Calendar.getInstance().getTime()));
+
+        String message = JSON.toJSONString(map);
         log.debug("#{}", message);
         // kafkaService.sendMessage(TopicEnum.SALES, message);
-        
+
         kafkaService.sendMessage(TopicEnum.ITEM, message);
-        return "已发送";
+        return "send success";
     }
 
     @RequestMapping(value = "leftnav", method = RequestMethod.GET)
